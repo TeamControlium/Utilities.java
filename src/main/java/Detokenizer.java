3ac709c34740a852daff8d7e145bfc5a39345157
Mat Walker <mat@alexview.com>
2018-02-28 08:07:40 +1000

    Added Detokenizer and started on Detokenizer unit tests

---
+++ src/main/java/Detokenizer.java
@@ -0,0 +1,421 @@
+import org.apache.commons.lang.StringUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+public class Detokenizer {
+
+    private static final char tokenStartChar = '{';
+    private static final char tokenEndChar = '}';
+
+    private static Random RandomGenerator = new Random();
+
+    public static String ProcessTokensInString(String stringWithTokens) throws Exception {
+        String detokenizedString = "";
+        int startIndex = 0;
+        boolean foundTokenStart = false;
+
+        //
+        // Find the start of a token, ignoring doubles {{'s as they are litterals)
+        //
+        while (!foundTokenStart && startIndex < stringWithTokens.length())
+        {
+            if (stringWithTokens.charAt(startIndex) == tokenStartChar)
+            {
+                // We are looking at a token start char...
+                if ((startIndex < stringWithTokens.length() - 1) && (stringWithTokens.charAt(startIndex + 1) == tokenStartChar))
+                {
+                    // Next char is also a start char, so ignore and skip past
+                    startIndex += 1;
+                }
+                else
+                {
+                    // Next char not a start char so we have found a token!
+                    foundTokenStart = true;
+                }
+            }
+            startIndex += 1;
+        }
+
+        //
+        // startIndex is now pointing to first char of a token
+        //
+
+        if (foundTokenStart)
+        {
+            boolean foundTokenEnd = false;
+            int endIndex = startIndex; // We start searching for the end of the token from the first character of the
+            //
+            // Find the end of the token.
+            //
+            while (!foundTokenEnd && endIndex < stringWithTokens.length())
+            {
+                if ((stringWithTokens.charAt(endIndex) == tokenStartChar) &&
+                        !((startIndex < stringWithTokens.length() - 1) && (stringWithTokens.charAt(startIndex + 1) == tokenStartChar)))
+                {
+                    //
+                    // Another start token (and it is NOT a dounble!!!!)  We have nested tokens by golly.
+                    // So, start the process again, but from the new start of the nested token. Hah, this
+                    // is a quick easy way of dealing with nested tokens!
+                    //
+                    startIndex = endIndex + 1;
+                }
+                else if (stringWithTokens.charAt(endIndex) == tokenEndChar)
+                {
+                    if ((endIndex < stringWithTokens.length() - 1) && (stringWithTokens.charAt(endIndex + 1) == tokenEndChar))
+                    {
+                        // Next char is also an end char, so ignore and skip past
+                        endIndex += 1;
+                    }
+                    else
+                    {
+                        // Next char not a start char so we have found a token!
+                        foundTokenEnd = true;
+                    }
+                }
+                endIndex += 1;
+            }
+            if (foundTokenEnd)
+            {
+                detokenizedString += stringWithTokens.substring(0, startIndex - 1);
+                String token = stringWithTokens.substring(startIndex, endIndex - 1);
+                try
+                {
+                    detokenizedString += ProcessToken(token);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(String.format("Error processing token {%s}: %s",token, ex));
+                }
+                detokenizedString += stringWithTokens.substring(endIndex, stringWithTokens.length());
+            }
+            else
+            {
+                throw new Exception(String.format("Found token start {{ found at index {%d} but no closing }} found: [%s]",startIndex,stringWithTokens));
+            }
+            // Now, we call ourself again to process any more tokens....
+            detokenizedString = ProcessTokensInString(detokenizedString);
+        }
+        else
+        {
+            // So no token found. We will convert all doubles back to singles and return the string...
+            detokenizedString += stringWithTokens.replace("{{", "{").replace("}}", "}");
+        }
+        return detokenizedString;
+    }
+
+    /// <summary>
+    /// System delegate to do custom token processing
+    /// </summary>
+    private static BiFunction<String,String[],String> _CustomTokenProcessor = null;
+    public static void setTestToolLog(BiFunction<String,String[],String> testToolLog) { _CustomTokenProcessor = testToolLog;}
+
+
+    private static String ProcessToken(String token) throws Exception {
+        String delimiter = ";";
+        String processedToken = "";
+        if (StringUtils.isEmpty(token)) throw new Exception("Empty token!");
+        String[] splitToken = token.split(delimiter, 2);
+        switch (splitToken[0].toLowerCase().trim())
+        {
+            case "random":
+                if (splitToken.length < 2) throw new Exception("Random token [" + token + "] needs 3 parts {{random;<type>;<length>}}");
+                processedToken = DoRandomToken(delimiter, splitToken[1]);
+                break;
+            case "date":
+                if (splitToken.length < 2) throw new Exception("Date token [" + token + "] needs 3 parts {{date;<offset>;<format>}}");
+                processedToken = DoDateToken(delimiter, splitToken[1]);
+                break;
+            case "financialyearstart":
+                if (splitToken.length < 2) throw new Exception("FinancialYearStart token [" + token + "] needs 3 parts {{FinancialYearStart;<date>;<format>}}");
+                processedToken = DoFinancialYearToken(delimiter, splitToken[1], true);
+                break;
+            case "financialyearend":
+                if (splitToken.length < 2) throw new Exception("FinancialYearEnd token [" + token + "] needs 3 parts {{FinancialYearEnd;<date>;<format>}}");
+                processedToken = DoFinancialYearToken(delimiter, splitToken[1], false);
+                break;
+ //           case "seleniumkeys":       // We dont want to have to reference Selenium from the Utilities classes
+ //           case "seleniumkey":
+ //               if (splitToken.length < 2) throw new Exception("SeleniumKey token [" + token + "] needs 2 parts {{SeleniumKey;<Name>}}");
+ //               processedToken = DoSeleniumKey(splitToken[1]);
+ //               break;
+            default: {
+                if (_CustomTokenProcessor == null) {
+                    throw new Exception("Unsupported token [" + splitToken[0] + "] in [" + token + "]");
+                }
+                else {
+                    try {
+                        processedToken = _CustomTokenProcessor.apply(delimiter, splitToken);
+                    }
+                    catch (Exception ex) {
+                        throw new Exception("Error thrown by custom token processor.",ex);
+                    }
+                    if (processedToken==null) {
+                        //
+                        // Custom token processor returns null if token not processed
+                        //
+                        throw new Exception("Unsupported token [" + splitToken[0] + "] in [" + token + "]");
+                    }
+                }
+            }
+        }
+        return processedToken;
+    }
+
+    private static String DoRandomToken(String delimiter, String TypeAndLength) throws Exception {
+        String[] typeAndLengthOrFormat = TypeAndLength.split(delimiter, 2);
+        String result ="";
+        String select = "";
+        String verb = typeAndLengthOrFormat[0].toLowerCase().trim();
+
+        if (verb.startsWith("date("))
+        {
+            result = new SimpleDateFormat(typeAndLengthOrFormat[1]).format(DoRandomDate(verb.substring(verb.indexOf('(') + 1, verb.length() - 1)));
+        }
+        else if (verb.startsWith("float("))
+        {
+            result = String.format(typeAndLengthOrFormat[1],DoRandomFloat(verb.substring(verb.indexOf('(') + 1, verb.length() - 1)));
+        }
+        else
+        {
+            // {random,from(ASDF),5} - 5 characters selected from ASDF
+            if (verb.startsWith("from("))
+            {
+                select = typeAndLengthOrFormat[0].trim().substring(verb.indexOf('(') + 1, verb.length() - 2 - verb.indexOf('('));
+            }
+            else
+            {
+                switch (verb)
+                {
+                    case "letters":
+                        select = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                        break;
+                    case "lowercaseletters":
+                        select = "abcdefghijklmnopqrstuvwxyz";
+                        break;
+                    case "uppercaseletters":
+                        select = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                        break;
+                    case "digits":
+                        select = "01234567890";
+                        break;
+                    case "alphanumerics":
+                        select = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
+                        break;
+                    case "acn":
+                    {
+                        String acn = ProcessTokensInString("{random;digits;9}");
+                        return acn;
+                    }
+                    case "abn":
+                    {
+                        String acn = ProcessTokensInString("{random;acn}");
+                        result = ProcessTokensInString("{{ABNFromACN;{acn}}}");
+                        return result;
+                    }
+                    default:
+                        throw new Exception("Unrecognised random Type [" +typeAndLengthOrFormat[0] +"] - Expect letters, lowercaseletters, uppercaseletters digits or alphanumerics");
+                }
+            }
+            int number;
+            try {
+                number = Integer.parseInt(typeAndLengthOrFormat[1]);
+            }
+            catch (Exception ex) {
+                throw new Exception("Invalid number of characters in Random token {{random;<type>;<length>}}");
+            }
+            for (int index = 0; index < number; index++) {
+                int selectIndex = ThreadLocalRandom.current().nextInt(0,select.length());
+                result += select.charAt(selectIndex);
+            }
+       }
+        return result;
+    }
+
+    static private float DoRandomFloat(String MaxAndMinFloats) throws Exception {
+        String delimiter = ",";
+        String[] MaxAndMin = MaxAndMinFloats.split(delimiter);
+        if (MaxAndMin.length != 2)
+            throw new Exception("Invalid Maximum and Minimum floats. Expect {{random.float(min;max),<format>}}. Max/min was: [" + MaxAndMinFloats + "]");
+        float Min;
+        float Max;
+
+        try {
+            Min = Float.parseFloat(MaxAndMin[0]);
+        }
+        catch (Exception ex) {
+            throw new Exception("Invalid Minimum float. Expect {{random.float(min;max),<format>}}. Max/min was: [" + MaxAndMinFloats +"]");
+        }
+        try {
+            Max = Float.parseFloat(MaxAndMin[1]);
+        }
+        catch (Exception ex) {
+            throw new Exception("Invalid Maximum float. Expect {{random.float(min;max),<format>}}. Max/min was: [" + MaxAndMinFloats +"]");
+        }
+        return DoRandomFloat(Min, Max);
+    }
+
+    static public float DoRandomFloat(float MinFloat, float MaxFloat) throws Exception {
+        if (MinFloat >= MaxFloat)
+            throw new Exception("Maximum float less than Minimum float! Expect {{random.float(min,max),<format>}} Min = "+ Float.toString(MinFloat) + ", Max = "+ Float.toString(MaxFloat));
+        return ThreadLocalRandom.current().nextFloat() * (MaxFloat - MinFloat) + MinFloat;
+    }
+
+    static private Date DoRandomDate(String MaxAndMinDates) throws Exception {
+        String delimiter = ",";
+        String[] MaxAndMin = MaxAndMinDates.split(delimiter);
+        if (MaxAndMin.length != 2)
+            throw new Exception("Invalid Maximum and Minimum dates. Expect {{random;date(dd-MM-yyyy,dd-MM-yyyy);<format>}}. Max/min was: [" + MaxAndMinDates + "]");
+        Date Min;
+        Date Max;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d-M-yyyy");
+
+        try {
+            Min = dateFormat.parse(MaxAndMin[0]);
+        }
+        catch (Exception ex) {
+            throw new Exception("Invalid Minimum date. Expect {{random;date(dd-MM-yyyy,dd-MM-yyyy);<format>}}. Max/min was: [" + MaxAndMinDates + "]");
+        }
+        try {
+            Max = dateFormat.parse(MaxAndMin[1]);
+        }
+        catch (Exception ex) {
+            throw new Exception("Invalid Maximum date. Expect {{random;date(dd-MM-yyyy,dd-MM-yyyy);<format>}}. Max/min was: [" + MaxAndMinDates + "]");
+        }
+
+        return DoRandomDate(Min, Max);
+    }
+    static public Date DoRandomDate(Date MinDate, Date MaxDate) throws Exception {
+        if (MinDate.after(MaxDate)) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("d-M-yyyy");
+            throw new Exception("Maximum date earlier than Maximum date! Expect {{random;date(dd-MM-yyyy,dd-MM-yyyy);<format>}} Mindate = " + dateFormat.format(MinDate) + ", Maxdate = " + dateFormat.format(MaxDate));
+        }
+        return new Date(ThreadLocalRandom.current().nextLong(MinDate.getTime(),MaxDate.getTime()));
+    }
+
+    private static String DoDateToken(String delimiter, String OffsetAndFormat) throws Exception {
+        String[] offsetAndFormat = OffsetAndFormat.split(delimiter, 2);
+
+        if (offsetAndFormat.length != 2)
+        {
+            throw new Exception("Date token does not have a format parameter; example: {date" + delimiter + "today" + delimiter + "dd-MM-yyyy}");
+        }
+        Date dt;
+        String verb = offsetAndFormat[0].toLowerCase().trim();
+        if (verb.startsWith("random("))
+        {
+            dt = DoRandomDate(verb.substring(verb.indexOf('(') + 1, verb.length() - 2 - verb.indexOf('(')));
+        }
+        else
+        {
+            switch (verb)
+            {
+                case "today":
+                    dt = new Date();
+
+                    break;
+                case "yesterday":
+                    dt = getDateOffset(Calendar.DATE,-1);
+                    break;
+                case "tomorrow":
+                    dt = getDateOffset(Calendar.DATE,+1);
+                    break;
+                default:
+                {
+                    String[] activeOffset = verb.substring(0, verb.length() - 1).split(Pattern.quote("("),2);
+                    if (offsetAndFormat[0].contains("(") && offsetAndFormat[0].endsWith(")"))
+                    {
+                        int offset;
+                        try {
+                            offset = Integer.parseInt(activeOffset[1]);
+                        }
+                         catch (Exception ex) {
+                             throw new Exception("Invalid Active Date offset.  Expect AddYears(n) AddMonths(n) or AddDays(n). Got [" + activeOffset[0].trim() + "]");
+                         }
+
+
+                        switch (activeOffset[0].trim())
+                        {
+                            case "addyears":
+                                dt = getDateOffset(Calendar.YEAR,offset);
+                                break;
+                            case "addmonths":
+                                dt = getDateOffset(Calendar.MONTH,offset);
+                                break;
+                            case "adddays":
+                                dt = getDateOffset(Calendar.DATE,offset);
+                                break;
+                            default:
+                                throw new Exception("Invalid Active Date offset.  Expect AddYears(n) AddMonths(n) or AddDays(n). Got [" + activeOffset[0].trim() + "]");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Invalid Active Date offset.  Expect AddYears(n) AddMonths(n) or AddDays(n). Got [" + activeOffset[0].trim() + "]");
+                    }
+                    break;
+                }
+            }
+        }
+        SimpleDateFormat dateFormat = new SimpleDateFormat(offsetAndFormat[1]);
+        return dateFormat.format(dt);
+    }
+
+    private static String DoFinancialYearToken(String delimiter, String DateToWorkFromAndFormat, boolean Start) throws Exception {
+        String financialYearStart = "01/07";
+        String financialYearEnd = "01/07";
+
+        String[] dateToWorkFromAndFormat = DateToWorkFromAndFormat.split(delimiter, 2);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+
+        Date dateToWorkFrom = tryParseDate(dateToWorkFromAndFormat[0], Arrays.asList("dd/MM/yyyy", "d/MM/yyyy", "dd/M/yyyy", "d/M/yyyy", "dd/MM/yy", "d/MM/yy", "dd/M/yy", "d/M/yy"));
+
+        if (dateToWorkFrom==null)
+        {
+            throw new Exception("Cannot parse date [" + dateToWorkFromAndFormat[0] + "].  Must be in format d/M/y.");
+        }
+
+        String year;
+        if (dateToCalendar(dateToWorkFrom).get(Calendar.MONTH)+1 >= 7)
+            year = Integer.toString(Start ? dateToCalendar(dateToWorkFrom).get(Calendar.YEAR) : dateToCalendar(dateToWorkFrom).get(Calendar.YEAR));
+        else
+            year = Integer.toString(Start ? (dateToCalendar(dateToWorkFrom).get(Calendar.YEAR) - 1) : dateToCalendar(dateToWorkFrom).get(Calendar.YEAR));
+
+        Date returnDate = tryParseDate((Start ? financialYearStart : financialYearEnd) + "/" + year,Arrays.asList("dd/MM/yyyy"));
+
+       return new SimpleDateFormat(dateToWorkFromAndFormat[1]).format(returnDate);
+    }
+
+    private static Date getDateOffset(int offsetType,int offset) {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(offsetType,offset);
+        return cal.getTime();
+    }
+
+    private static Calendar dateToCalendar(Date date) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
+    }
+
+
+    private static Date tryParseDate(String dateString, List<String> validFormats) {
+        for (String formatString : validFormats) {
+            try {
+                return new SimpleDateFormat(formatString).parse(dateString);
+            }
+            catch (ParseException ex) {
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ src/test/java/DetokenizerTest.java
@@ -0,0 +1,176 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetokenizerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyTodaysDateToken() {
+        String detokenised="<Not Set>";
+        try {
+             detokenised = Detokenizer.ProcessTokensInString("{date;today;dd/MM/yyyy}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+        Date dt = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        assertEquals(dateFormat.format(dt),detokenised,"Date returned by Detokeniser matches todays date and in correct format");
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyYesterdaysDateToken() {
+        String detokenised="<Not Set>";
+        try {
+            detokenised = Detokenizer.ProcessTokensInString("{date;yesterday;dd/MM/yyyy}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE,-1);
+        Date dt =  cal.getTime();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        assertEquals(dateFormat.format(dt),detokenised,"Date returned by Detokeniser matches yesterdays date and in correct format");
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyRandomDateToken() {
+        String detokenised="<Not Set>";
+        String startDate = "01-01-1975";
+        String endDate = "31-12-1975";
+        SimpleDateFormat returnedDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+        try {
+            detokenised = Detokenizer.ProcessTokensInString("{random;date(" + startDate + "," + endDate + ");dd/MM/yyyy}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+
+        Date returnedDate=null;
+        try {
+             returnedDate = returnedDateFormat.parse(detokenised);
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception parsing [Detokenizer.ProcessTokensInString] returned date [" + detokenised + "]: " + ex);
+        }
+
+        Date startDateDate=null;
+        Date endDateDate=null;
+        try {
+            startDateDate = inputDateFormat.parse(startDate);
+            endDateDate = inputDateFormat.parse(endDate);
+        }
+        catch (Exception ex) {
+            assertTrue(false, "Test error parsing start or end dates!");
+        }
+
+        assertTrue((!returnedDate.before(startDateDate)) && !returnedDate.after(endDateDate),"Verify returned date from token {random;date("+startDate+","+endDate+");dd/MM/yyyy} is not outside the minimum and maximum dates");
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyRandomDigits() {
+        String detokenised="<Not Set>";
+        int requiredLength = 5;
+        try {
+            detokenised = Detokenizer.ProcessTokensInString("{random;digits;"+ Integer.toString(requiredLength)+"}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+
+        boolean canParseInteger=false;
+        try {
+            int dummy = Integer.parseInt(detokenised);
+            canParseInteger=true;
+        }
+        catch (Exception ex) {}
+
+        assertTrue(canParseInteger,"Verify only digits returned");
+        assertEquals(requiredLength,detokenised.length(),"Verify length of returned value is as required");
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyRandomLetters() {
+        String detokenised="<Not Set>";
+        int requiredLength = 20;
+        try {
+            detokenised = Detokenizer.ProcessTokensInString("{random;letters;"+ Integer.toString(requiredLength)+"}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+
+
+        assertTrue(detokenised.matches("[a-zA-Z]+"),"Verify only letters returned");
+        assertEquals(requiredLength,detokenised.length(),"Verify length of returned value is as required");
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyRandomUppercaseLetters() {
+        String detokenised="<Not Set>";
+        int requiredLength = 20;
+        try {
+            detokenised = Detokenizer.ProcessTokensInString("{random;UpperCaseLetters;"+ Integer.toString(requiredLength)+"}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+
+
+        assertTrue(detokenised.matches("[A-Z]+"),"Verify only uppercase letters returned");
+        assertEquals(requiredLength,detokenised.length(),"Verify length of returned value is as required");
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyRandomLowercaseLetters() {
+        String detokenised="<Not Set>";
+        int requiredLength = 20;
+        try {
+            detokenised = Detokenizer.ProcessTokensInString("{random;LowerCaseLetters;"+ Integer.toString(requiredLength)+"}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+
+
+        assertTrue(detokenised.matches("[a-z]+"),"Verify only lowercase letters returned");
+        assertEquals(requiredLength,detokenised.length(),"Verify length of returned value is as required");
+    }
+
+    @org.junit.jupiter.api.Test
+    void VerifyRandomSpecificCharacters() {
+        String detokenised="<Not Set>";
+        String characterSet = "a1\\b2c3d4!@#$%^&*()";  // Make sure we include characters that may cause issues!
+        int requiredLength = 100;
+        try {
+            detokenised = Detokenizer.ProcessTokensInString("{random;from("+characterSet+");"+ Integer.toString(requiredLength)+"}");
+        }
+        catch (Exception ex) {
+            assertTrue(false,"Exception calling [Detokenizer.ProcessTokensInString]: " + ex);
+        }
+
+
+        assertTrue(detokenised.matches("["+ Pattern.quote(characterSet)+"]+"),"Verify only characters from character set returned");
+        assertEquals(requiredLength,detokenised.length(),"Verify length of returned value is as required");
+    }
+}
